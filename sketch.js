06097283bbafd582645e79e5e48195de8f6a6105
usselman <sarkar.shirish@gmail.com>
2023-10-04 10:15:28 -0700

    update, brush size changes

--- sketch.js
+++ sketch.js
@@ -12,14 +12,28 @@ let emojis = [
 ];
 
 let currentEmojiIndex = 0;
-let captureScreenshot = false;
+let brushSize = 30;
+let textColor = [0, 0, 0]; // RGB
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(255);
     frameRate(100);
     textSize(windowWidth / 42);
-    //text("Press 'i' for instructions", 10, 60);
+    textSize(25);
+    fill(textColor);
+    for (let i = 0; i < emojis.length; i++) {
+        text(String.fromCodePoint(emojis[i]), 10 + (i * 50), 30);
+    }
+    text("Click to draw.", 10, 80);
+    text("Press BACKSPACE to clear.", 10, 130);
+    text("Press 's' to switch emoji.", 10, 180);
+    text("Press ENTER to save screenshot.", 10, 230);
+    text("Press 'i' to show emoji list.", 10, 280);
+    text("Press 'z' to increase brush size, 'x' to decrease.", 10, 330);
+    text("Press 'c' to hide text", 10, 380);
+    //text("Currently selected emoji: " + String.fromCodePoint(emojis[currentEmojiIndex]), 10, 310);
+    text("Clear screen of text before screenshotting!", 10, 430);
 }
 
 function windowResized() {
@@ -27,31 +41,14 @@ function windowResized() {
 }
 
 function draw() {
-    if (!captureScreenshot) {
-        // Draw the text only if we're not capturing a screenshot
-        // textSize(55);
-        text("Drag to draw, click to create space.", 10, 60);
-        text("Press BACKSPACE to clear.", 10, 110);
-        text("Press 'n' to switch emoji.", 10, 160);
-        text("Press ENTER to save screenshot.", 10, 210)
-        text("Press 'i' to bring this back.", 10, 260);
-        for (let i = 0; i < emojis.length; i++) {
-            text(String.fromCodePoint(emojis[i]), 10 + (i * 50), 310);
-        }
-        //text("Currently selected emoji: " + String.fromCodePoint(emojis[currentEmojiIndex]), 10, 310);
-        text("Clear screen of text before screenshotting!", 10, 360);
-    }
+    // Draw the text only if we're not capturing a screenshot
 }
 
 function keyPressed() {
     if (keyCode === ENTER) {
-        //captureScreenshot = true;
-        // Set the flag to true before capturing
-        //background(255); // Clear the canvas to remove text
         saveCanvas('paintmoji', 'png'); // Capture the screenshot
-        //captureScreenshot = false; // Reset the flag
     }
-    if (key === 'n') {
+    if (key === 's') {
         // Switch to the next emoji in the array
         currentEmojiIndex = (currentEmojiIndex + 1) % emojis.length;
     }
@@ -59,14 +56,21 @@ function keyPressed() {
         // Clear the canvas
         background(255);
     }
+    if (key === 'z') {
+        brushSize += 5;
+    }
+    if (key === 'x') {
+        brushSize = max(5, brushSize - 5);
+    }
+    if (key === 'c') {
+        textColor = [random(255), random(255), random(255)];
+    }
     if (keyIsDown(73)) {
-        text("Drag to draw, click to create space.", 10, 60);
-        text("Press BACKSPACE to clear.", 10, 110);
-        text("Press 'n' to switch emoji.", 10, 160);
-        text("Press ENTER to save screenshot.", 10, 210)
-        text("Press 'i' to bring this back.", 10, 260);
-        text("Currently selected emoji: " + String.fromCodePoint(emojis[currentEmojiIndex]), 10, 310);
-        text("Clear screen of text before screenshotting!", 10, 360);
+        textSize(25);
+        fill(textColor);
+        for (let i = 0; i < emojis.length; i++) {
+            text(String.fromCodePoint(emojis[i]), 10 + (i * 50), 30);
+        }
     }
 }
 
@@ -74,8 +78,7 @@ function keyIsDown() {
 
 }
 
-function mouseMoved() {
-    captureScreenshot = true;
-    // Draw the emoji
+function mouseDragged() {
+    textSize(brushSize);
     text(String.fromCodePoint(emojis[currentEmojiIndex]), mouseX, mouseY);
 }
