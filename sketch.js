6e8e4261b46c6d03b59578e97d89393d3199cc5b
usselman <sarkar.shirish@gmail.com>
2023-10-06 12:45:22 -0700

    Redo/Undo feature + DOM buttons/text

--- sketch.js
+++ sketch.js
@@ -15,6 +15,18 @@ let currentEmojiIndex = 0;
 let brushSize = 30;
 let textColor = [0, 0, 0]; // RGB
 
+let snapshots = []; // Array to hold snapshots of the canvas
+let currentSnapshotIndex = -1; // Index to keep track of the current snapshot
+
+/*
+* DOM BUTTONS
+*/
+let undoButton;
+let redoButton;
+let saveButton;
+let clearButton;
+let emojiButton;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(255);
@@ -22,18 +34,42 @@ function setup() {
     textSize(windowWidth / 42);
     textSize(25);
     fill(textColor);
-    for (let i = 0; i < emojis.length; i++) {
-        text(String.fromCodePoint(emojis[i]), 10 + (i * 50), 30);
-    }
-    text("Click to draw.", 10, 80);
-    text("Press BACKSPACE to clear.", 10, 130);
-    text("Press 's' to switch emoji.", 10, 180);
-    text("Press ENTER to save screenshot.", 10, 230);
-    text("Press 'i' to show emoji list.", 10, 280);
-    text("Press 'z' to increase brush size, 'x' to decrease.", 10, 330);
-    text("Press 'c' to change text color", 10, 380);
-    //text("Currently selected emoji: " + String.fromCodePoint(emojis[currentEmojiIndex]), 10, 310);
-    text("Clear screen of text before screenshotting!", 10, 430);
+    displayButtons();
+    //displayInstructions();
+    takeSnapshot();
+}
+
+function displayButtons() {
+    //textSize(50);
+    undoButton = createButton('Undo');
+    undoButton.size(60, 20);
+    undoButton.position(windowWidth / 2 - 80, 50);
+    undoButton.mousePressed(undo);
+
+    redoButton = createButton('Redo');
+    redoButton.size(60, 20);
+    redoButton.position(windowWidth / 2 + 80, 50);
+    redoButton.mousePressed(redo);
+}
+
+function displayInstructions() {
+    // for (let i = 0; i < emojis.length; i++) {
+    //     text(String.fromCodePoint(emojis[i]), 10 + (i * 50), 30);
+    // }
+    let emojiSpan = createSpan(`Currently selected emoji: ${String.fromCodePoint(emojis[currentEmojiIndex])}`);
+    emojiSpan.position(20, 50);
+    let instructionSpan = createSpan(`
+    Click to draw! <br/> 
+    Press BACKSPACE to clear <br/> 
+    Press 's' to switch emoji. <br/> 
+    Press ENTER to save screenshot. <br/> 
+    Press 'z' to increase brush size <br/> 
+    Press 'c' to change text color <br/> 
+    Press 'u' to undo, 'r' to redo <br/> 
+    (Instructions and buttons do not render in screenshot) <br/> 
+    `);
+    instructionSpan.position(20, 80);
+
 }
 
 function windowResized() {
@@ -41,7 +77,7 @@ function windowResized() {
 }
 
 function draw() {
-    // Draw the text only if we're not capturing a screenshot
+    displayInstructions();
 }
 
 function keyPressed() {
@@ -66,19 +102,41 @@ function keyPressed() {
         textColor = [random(255), random(255), random(255)];
     }
     if (keyIsDown(73)) {
-        textSize(25);
-        fill(textColor);
-        for (let i = 0; i < emojis.length; i++) {
-            text(String.fromCodePoint(emojis[i]), 10 + (i * 50), 30);
-        }
+        displayInstructions();
     }
-}
-
-function keyIsDown() {
-
+    if (key === 'u') {
+        undo();
+    }
+    if (key === 'r') {
+        redo();
+    }
 }
 
 function mouseDragged() {
     textSize(brushSize);
     text(String.fromCodePoint(emojis[currentEmojiIndex]), mouseX, mouseY);
+    takeSnapshot();
+}
+
+function takeSnapshot() {
+    let snapshot = get(); // Capture the current state of the canvas
+    if (currentSnapshotIndex < snapshots.length - 1) {
+        snapshots.splice(currentSnapshotIndex + 1); // Remove 'future' snapshots if any
+    }
+    snapshots.push(snapshot);
+    currentSnapshotIndex++;
+}
+
+function undo() {
+    if (currentSnapshotIndex > 0) {
+        currentSnapshotIndex--;
+        image(snapshots[currentSnapshotIndex], 0, 0); // Display the previous snapshot
+    }
+}
+
+function redo() {
+    if (currentSnapshotIndex < snapshots.length - 1) {
+        currentSnapshotIndex++;
+        image(snapshots[currentSnapshotIndex], 0, 0); // Display the next snapshot
+    }
 }
